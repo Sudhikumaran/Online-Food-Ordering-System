97e4f93f2e6601c4715dc7c88f800b4826e34717
Sudhi Kumaran <sudhikumaran2005@gmail.com>
2026-01-05 09:25:26 +0530

    Order.py has been updated

--- Order.py
+++ Order.py
@@ -1,7 +1,10 @@
+from OrderStatus import OrderStatus
+
 class Order:
-    def __init__(self,items):
+    def __init__(self, items):
         self._items = items
         self._total_amount = 0
+        self._status = OrderStatus.CREATED
         self._calculate_total()
 
     def _calculate_total(self):
@@ -9,7 +12,15 @@ class Order:
             self._total_amount += item.get_price()
 
     def confirm_order(self):
-        print(f"Order confirmed. Amount : {self._total_amount}")
-    
+        self._status = OrderStatus.CONFIRMED
+        print(f"Order confirmed. Amount: {self._total_amount}")
+
+    def mark_paid(self):
+        self._status = OrderStatus.PAID
+        print("Order payment successful")
+
     def get_total_amount(self):
         return self._total_amount
+
+    def get_status(self):
+        return self._status
